62de08830090c8af77b7ade33f457239448f4474
SPStore <libo@LibodeMacBook-Pro.local>
2017-11-08 17:13:18 +0800

    优化爱奇艺demo

--- HVScrollView(仿爱奇艺)/HVScrollView(仿爱奇艺)/ViewController.m
+++ HVScrollView(仿爱奇艺)/HVScrollView(仿爱奇艺)/ViewController.m
@@ -25,8 +25,6 @@
 @property (nonatomic, strong) MyHeaderView *headerView;
 @property (nonatomic, strong) SPPageMenu *pageMenu;
 
-@property (nonatomic, assign) NSInteger selectedIndex;
-
 @property (nonatomic, assign) CGFloat lastPageMenuY;
 
 @property (nonatomic, assign) CGPoint lastPoint;
@@ -75,13 +73,13 @@
 
     if (scrollView == self.scrollView) {
         // 将当前控制器的view带到最前面去，这是为了防止下一个控制器的view挡住了头部
-        BaseViewController *baseVc = self.childViewControllers[_selectedIndex];
+        BaseViewController *baseVc = self.childViewControllers[self.pageMenu.selectedItemIndex];
         if ([baseVc isViewLoaded]) {
             [self.scrollView bringSubviewToFront:baseVc.view];
         }
         // 横向切换tableView时头部不要跟随tableView偏移
         CGRect headerFrame = self.headerView.frame;
-        headerFrame.origin.x = scrollView.contentOffset.x-kScreenW*_selectedIndex;
+        headerFrame.origin.x = scrollView.contentOffset.x-kScreenW*self.pageMenu.selectedItemIndex;
         self.headerView.frame = headerFrame;
         
         [self configerHeaderY];
@@ -94,23 +92,16 @@
         });
         
     }
-    
 }
-
 
 - (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView {
     if (scrollView == self.scrollView) {
         NSInteger index = scrollView.contentOffset.x / scrollView.frame.size.width;
         // 手动滑scrollView,pageMenu会根据传进去的index选中index对应的button
-        [self.pageMenu selectButtonAtIndex:index];
+        self.pageMenu.selectedItemIndex = index;
         
         [self configerHeaderY];
-        
-        BaseViewController *baseVc = self.childViewControllers[_selectedIndex];
-        // 如果scrollView的内容很少，在屏幕范围内，则自动回落
-        if (baseVc.scrollView.contentSize.height < kScreenH && [baseVc isViewLoaded]) {
-            [baseVc.scrollView setContentOffset:CGPointMake(0, 0) animated:YES];
-        }
+
     }
 }
 
@@ -123,15 +114,15 @@
     
     CGFloat distanceY;
     
-    // 取出的scrollingScrollView并非是唯一的，当有多个子控制器上的scrollView同时滑动时都会发出通知来到这个方法，所以要过滤
-    BaseViewController *baseVc = self.childViewControllers[_selectedIndex];
+    BaseViewController *baseVc = self.childViewControllers[self.pageMenu.selectedItemIndex];
     
+    // 取出的scrollingScrollView并非是唯一的，当有多个子控制器上的scrollView同时滑动时都会发出通知来到这个方法，所以要过滤
     if (scrollingScrollView == baseVc.scrollView && baseVc.isFirstViewLoaded == NO) {
         // 让悬浮菜单跟随scrollView滑动
         CGRect pageMenuFrame = self.pageMenu.frame;
 
         if (pageMenuFrame.origin.y >= kNaviH) {
-            // 往上移
+            // 往上滑
             if (offsetDifference > 0) {
                 
                 if (((scrollingScrollView.contentOffset.y+self.pageMenu.frame.origin.y)>=kHeaderViewH) || scrollingScrollView.contentOffset.y < 0) {
@@ -142,7 +133,7 @@
                     }
                     
                 }
-            } else { // 往下移
+            } else { // 往下滑
                 if ((scrollingScrollView.contentOffset.y+self.pageMenu.frame.origin.y)-kNaviH<kHeaderViewH) {
                     pageMenuFrame.origin.y = -scrollingScrollView.contentOffset.y+kHeaderViewH+kNaviH;
                 }
@@ -183,7 +174,7 @@
 // 此方法是难点
 - (void)configerHeaderY {
     // 取出当前子控制器
-    BaseViewController *baseVc = self.childViewControllers[_selectedIndex];
+    BaseViewController *baseVc = self.childViewControllers[self.pageMenu.selectedItemIndex];
     CGRect headerFrame = self.headerView.frame;
     // 将pageMenu的frame转换到当前正在滑动的scrollView上去（这一步很关键）
     CGRect pageMenuFrameInScrollView = [self.pageMenu convertRect:self.pageMenu.bounds toView:baseVc.scrollView];
@@ -193,8 +184,8 @@
 }
 
 #pragma mark - SPPageMenuDelegate
-- (void)pageMenu:(SPPageMenu *)pageMenu buttonClickedFromIndex:(NSInteger)fromIndex toIndex:(NSInteger)toIndex {
-    _selectedIndex = toIndex;
+- (void)pageMenu:(SPPageMenu *)pageMenu itemSelectedFromIndex:(NSInteger)fromIndex toIndex:(NSInteger)toIndex {
+
     // 如果上一次点击的button下标与当前点击的buton下标之差大于等于2,说明跨界面移动了,此时不动画.
     if (labs(toIndex - fromIndex) >= 2) {
         // 如果动画为NO,则会立即调用scrollViewDidScroll，scrollViewDidScroll中做了一个很重要的操作：改变每个tableView的头视图的frame，如果先调scrollViewDidScroll,然后继续来到此方法走完剩余代码，走到targetViewController.headerView = self.headerView这一行时，内部把头视图的origin都归0了，所以有时会导致头视图的origin为(0,0)的情况，为了避免这个问题，有一种办法是设置动画为YES，如果有动画，则会把本方法先走完，再去调scrollViewDidScroll,这便不会导致头视图origin为0的情况，但是这里用动画不太好看，所以固定在主线程
@@ -204,7 +195,7 @@
     } else {
         [self.scrollView setContentOffset:CGPointMake(_scrollView.frame.size.width * toIndex, 0) animated:YES];
     }
-    
+    if (!self.childViewControllers.count) {return;}
     BaseViewController *targetViewController = self.childViewControllers[toIndex];
     targetViewController.headerView = self.headerView;
     // 如果已经加载过，就不再加载
@@ -256,16 +247,14 @@
 - (SPPageMenu *)pageMenu {
     
     if (!_pageMenu) {
-        _pageMenu = [SPPageMenu pageMenuWithFrame:CGRectMake(0, CGRectGetMaxY(self.headerView.frame)+kNaviH, kScreenW, kPageMenuH) array:@[@"第一个",@"第二个",@"第三个",@"第四个"]];
-        _pageMenu.backgroundColor = [UIColor whiteColor];
+        _pageMenu = [SPPageMenu pageMenuWithFrame:CGRectMake(0, CGRectGetMaxY(self.headerView.frame)+kNaviH, kScreenW, kPageMenuH) trackerStyle:SPPageMenuTrackerStyleLineLongerThanItem];
+        [_pageMenu setItems:@[@"第一页",@"第二页",@"第三页",@"第四页"] selectedItemIndex:0];
         _pageMenu.delegate = self;
-        _pageMenu.buttonFont = [UIFont systemFontOfSize:16];
-        _pageMenu.selectedTitleColor = [UIColor blackColor];
-        _pageMenu.unSelectedTitleColor = [UIColor colorWithWhite:0 alpha:0.6];
-        _pageMenu.trackerColor = [UIColor orangeColor];
-        _pageMenu.firstButtonX = 15;
-        _pageMenu.allowBeyondScreen = NO;
-        _pageMenu.equalWidths = NO;
+        _pageMenu.itemTitleFont = [UIFont systemFontOfSize:16];
+        _pageMenu.selectedItemTitleColor = [UIColor blackColor];
+        _pageMenu.unSelectedItemTitleColor = [UIColor colorWithWhite:0 alpha:0.6];
+        _pageMenu.tracker.backgroundColor = [UIColor orangeColor];
+        _pageMenu.permutationWay = SPPageMenuPermutationWayNotScrollEqualWidths;
         
     }
     return _pageMenu;
